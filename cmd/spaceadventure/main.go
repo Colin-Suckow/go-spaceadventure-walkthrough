fa034ebf4968b03b4ed9ed68c6d579c60374b376
Colin Suckow <colin@suckow.dev>
2019-11-13 10:14:54 -0800

    Add planets argument

--- cmd/spaceadventure/main.go
+++ cmd/spaceadventure/main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/colinsuckow/spaceadventure_walkthrough/internal/spaceadventure"
 
 func main() {
-	spaceadventure.Start(spaceadventure.PlanetarySystem{Name: "Solar System"})
+	spaceadventure.Start(spaceadventure.PlanetarySystem{Name: "Solar System", Planets: []spaceadventure.Planet{}})
 }
